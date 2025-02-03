f7f69d351788ba32b9de25ca20983861d6ebea95
sandrovec <sandrovec@gmail.com>
2025-02-02 19:19:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 import requests
 
@@ -5,7 +6,7 @@ app = Flask(__name__)
 
 # Configuración de la API de Moodle
 MOODLE_URL = "https://escueladeartesonoras.com/moodle/webservice/rest/server.php"
-TOKEN = "2373eab4985d0366690868436e053e1f"
+TOKEN = os.getenv("MOODLE_TOKEN")  # Asegúrate de establecer esta variable de entorno
 
 @app.route('/get_courses', methods=['GET'])
 def get_courses():
@@ -27,10 +28,13 @@ def get_courses():
     response = requests.get(MOODLE_URL, params=params)
 
     if response.status_code == 200:
-        return jsonify(response.json())  # Devuelve los cursos del alumno
+        try:
+            return jsonify(response.json())  # Devuelve los cursos del alumno
+        except ValueError:
+            return jsonify({"error": "Error al procesar la respuesta de la API"}), 500
     else:
         return jsonify({"error": "No se pudo obtener los cursos"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto proporcionado por Render
+    app.run(debug=True, host='0.0.0.0', port=port)
